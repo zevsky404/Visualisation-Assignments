4105273ba802f0b3dfba6dcec78ae74d6b1d6477
zevsky404 <chrissiguetter2001@gmail.com>
2023-06-12 22:51:06 +0200

    implement tree map and add wacky text

--- Assignment-4/treemap.js
+++ Assignment-4/treemap.js
@@ -1,5 +1,6 @@
 import * as d3 from "d3";
 import { bigMoneyFormat, shortenText } from "./src/utils.js";
+import {stratify, treemapDice, treemapSlice, treemapSliceDice} from "d3";
 
 export function treemap({
   svg,
@@ -16,8 +17,15 @@ export function treemap({
 
 
   // INSERT YOUR CODE HERE
-  // const root = ...
+  const root = d3.hierarchy(data)
+      .sum(entry => entry.revenue)
+      .sort(function(a, b) { return b.height - a.height || b.value - a.value; });
 
+  const treemap = d3.treemap()
+      .size([width, height])
+      .padding(3)
+      .tile(treemapSliceDice)
+      (root);
 
   // END OF YOUR CODE
 
@@ -30,23 +38,87 @@ export function treemap({
     if (typeof (root) === "undefined") return;
 
     // create a group for each leaf node
-    // const leaf = ...
+    const node = svg
+        .selectAll("g")
+        .data(root.leaves())
+        .join("g")
+        //.attr("transform", (d) => `translate(${d.y0}, ${d.x0})`);
 
+    const getTopMostParent = (d) => {
+      const ancestors = d.ancestors();
+      const topMostParent = ancestors[ancestors.length - 2].data;
+      return topMostParent;
+    }
 
     // actually draw the rectangles
-    // leaf ...
+    node.each(function () {
+      d3.select(this)
+          .append("rect")
+          .attr("class", "treemap-rect")
+          .style("fill", data => { return color(getTopMostParent(data)); })
+          .attr("x", d => { return d.x0; })
+          .attr("y", d => { return d.y0; })
+          .attr("width", data => { return data.x1 - data.x0})
+          .attr("height", data => { return data.y1 - data.y0; })
+
+    });
+
 
     // setup labels for rectangles that are big enough
-    // const text = ...
+    const text = (data) => {
+      let movieTitle;
+      let movieRevenue;
+      data.data.name ? movieTitle = shortenText(data.data.name, 30) : movieTitle = shortenText(data.data.title, 15);
+      movieRevenue = bigMoneyFormat(data.value);
+
+      return `${movieTitle}: \n ${movieRevenue}`
+    }
 
     // add the name and the value as labels
-    // text
+    node.each(function() {
+      d3.select(this)
+          .append("text")
+          .attr("class", "cell-title")
+          .text(data => { return text(data); })
+          .attr("font-size", data => { return fontSize(data); })
+          .attr("x", d => { return d.x1; })
+          .attr("y", d => { return d.y0; });
 
-  }
+      const rect = d3.select(this).select("rect");
+      const textElement = d3.select(this).select("text");
+
+      const rectHeight = rect.attr("height");
+      const rectWidth = rect.attr("width");
+      const textWidth = textElement.node().getComputedTextLength();
+      const textHeight = textElement.attr("font-size") * 0.9;
+
+      if (rectHeight < textHeight) {
+        d3.select(this).select("text").classed("hidden", true).classed("cell-title", false);
+      }
+
+      /*if (rectWidth < textWidth) {
+        d3.select(this).select("text").classed("hidden", true).classed("cell-title", false);
+      }*/
+
+  });
+
+    const translation = (d, i, nodes) => {
+      const fontSize = parseFloat(d3.select(nodes[i]).style("font-size"));
+      const translateY = fontSize * 0.9;
+      return `translate(5, ${translateY}), rotate(-90, ${d.x0}, ${d.y0})`;
+    }
+
+    const allTextElements = d3.selectAll("text.cell-title");
+
+    allTextElements.each(function () {
+      d3.select(this)
+          .attr("transform", translation)
+    });
 }
 
 // naive function to heuristically determine font size based on the rectangle size
-function fontSize(d) {
-  return Math.min(12, Math.max(8, d.x1 - d.x0 - 4));
+  function fontSize(d) {
+    return Math.min(12, Math.max(8, d.x1 - d.x0 - 4));
+  }
 }
 
